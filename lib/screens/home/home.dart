d8824606a8638933d603b5837a15be925d7b5266
Balogun Murtadho <murtadhob@gmail.com>
2024-03-05 18:07:51 +0100

    Home screen completed

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task_manager/constants/asset_path.dart';
 import 'package:task_manager/constants/color_constants.dart';
