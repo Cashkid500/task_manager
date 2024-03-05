2025b842687cca08a0176b44fda7913ad1fad2ae
Balogun Murtadho <murtadhob@gmail.com>
2024-03-05 17:58:31 +0100

    Created a variable constants for texts to ensure code readability and maintainability

--- lib/constants/text_constants.dart
+++ lib/constants/text_constants.dart
@@ -2,6 +2,29 @@ class TaskManagerText {
 //*****************  Splash Screen Texts *************************
   static const String taskmanager = "Task Manager";
 
-//*****************  Onboarding Texts *************************
+//*****************  Home Texts *************************
+  static const String task = "Task";
+  static const String manager = "Manager";
+  static const String pending = "You have [8] pending task out of [8]";
+  static const String api = "Task Manager API";
+  static const String handles = "An API that handles user's tasks";
+  static const String date = "05/03/2024";
 
+//*****************  Add Task Texts *************************
+  static const String addTask = "Add Task";
+  static const String add = "Add";
+  static const String title = "Title";
+  static const String description = "Description";
+
+//*****************  History Texts *************************
+  static const String history = "History";
+  static const String completed = "You have completed [1] tasks";
+  static const String taskReassigned = "Task reassigned";
+
+//*****************  Settings Texts *************************
+  static const String settings = "Settings";
+  static const String version = "Version 1.0";
+  static const String clear = "CLEAR ALL DATA";
+  static const String terms = "Terms and Condition";
+  static const String privacy = "Privacy Policy";
 }
