045cf971254c3adeafb6871a2ae2979b0df57bbc
Balogun Murtadho <murtadhob@gmail.com>
2024-03-05 18:03:30 +0100

    Settings screen completed

---
+++ lib/screens/settings/settings.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task_manager/constants/asset_path.dart';
+import 'package:task_manager/constants/color_constants.dart';
+import 'package:task_manager/constants/text_constants.dart';
+import 'package:task_manager/screens/home/home.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.only(left: 20.sp, top: 20.sp, right: 20.sp),
+            child: Row(
+              children: [
+                GestureDetector(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen())),
+                    child: const Icon(Icons.arrow_back_ios)),
+
+                SizedBox(width: 30.sp),
+
+                //*********  Settings Text *********/
+                Text(
+                  TaskManagerText.settings,
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.normal,
+                    fontFamily: TaskManagerAssetsPath.taskManagerFont,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 80.sp),
+
+          //********* Logo *********/
+          Container(
+            height: 80.sp,
+            width: 80.sp,
+            decoration: BoxDecoration(
+              color: whiteText,
+              borderRadius: BorderRadius.circular(100.sp),
+            ),
+            child: Icon(
+              Icons.done_sharp,
+              size: 50.sp,
+              color: greenTeal,
+            ),
+          ),
+
+          SizedBox(
+            height: 10.sp,
+          ),
+
+          Text(
+            TaskManagerText.taskmanager,
+            style: TextStyle(
+              fontSize: 25.sp,
+              fontWeight: FontWeight.w600,
+              color: Colors.grey,
+              fontFamily: TaskManagerAssetsPath.taskManagerFont,
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          Text(
+            TaskManagerText.version,
+            style: TextStyle(
+              fontSize: 15.sp,
+              fontWeight: FontWeight.normal,
+              color: Colors.grey,
+              fontFamily: TaskManagerAssetsPath.taskManagerFont,
+            ),
+          ),
+
+          const Dividing(),
+
+          //*********  Clear Data Button *********/
+          Container(
+            height: 40.sp,
+            width: 250.sp,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(10.sp),
+            ),
+            child: Center(
+              child: Text(
+                TaskManagerText.clear,
+                style: TextStyle(
+                  fontSize: 15.sp,
+                  color: whiteText,
+                  fontFamily: TaskManagerAssetsPath.taskManagerFont,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+          ),
+
+          SizedBox(
+            height: 70.sp,
+            width: 310.sp,
+            child: Divider(
+              color: Colors.grey,
+              thickness: 1.5.sp,
+            ),
+          ),
+
+          //*********  Terms and Condition *********/
+          Padding(
+            padding: EdgeInsets.only(right: 63.sp),
+            child: Text(
+              TaskManagerText.terms,
+              style: TextStyle(
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w700,
+                color: Colors.brown,
+                fontFamily: TaskManagerAssetsPath.taskManagerFont,
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20.sp),
+
+          //*********  Privacy Policy *********/
+          Padding(
+            padding: EdgeInsets.only(right: 120.sp),
+            child: Text(
+              TaskManagerText.privacy,
+              style: TextStyle(
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w700,
+                color: Colors.brown,
+                fontFamily: TaskManagerAssetsPath.taskManagerFont,
+              ),
+            ),
+          ),
+
+          SizedBox(
+            height: 50.sp,
+            width: 310.sp,
+            child: Divider(
+              color: Colors.grey,
+              thickness: 1.5.sp,
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class Dividing extends StatelessWidget {
+  const Dividing({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100.sp,
+      width: 310.sp,
+      child: Divider(
+        color: Colors.grey,
+        thickness: 1.5.sp,
+      ),
+    );
+  }
+}
