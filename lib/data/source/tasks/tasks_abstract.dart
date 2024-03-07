11164c7bdf08ad985f5dbd501ca4c18513ff7e56
Cashkid500 <murtadhob@gmail.com>
2024-03-07 17:16:02 +0100

    Abstract files created

---
+++ lib/data/source/tasks/tasks_abstract.dart
@@ -0,0 +1,27 @@
+import 'package:task_manager/models/tasks/body/create_task.dart';
+import 'package:task_manager/models/tasks/body/delete_task.dart';
+import 'package:task_manager/models/tasks/body/update_task.dart';
+import 'package:task_manager/models/tasks/response/create_task.dart';
+import 'package:task_manager/models/tasks/response/delete_task.dart';
+import 'package:task_manager/models/tasks/response/update_task.dart';
+
+//************ Create Task **********
+abstract class CreateTaskSource {
+  Future<CreateTaskResponse> createTask({
+    required CreateTaskModel payload,
+  });
+}
+
+//************ Update Task **********
+abstract class UpdateTaskSource {
+  Future<UpdateTaskResponse> updateTask({
+    required UpdateTaskModel payload,
+  });
+}
+
+//************ Delete Task **********
+abstract class DeleteTaskSource {
+  Future<DeletetaskResponse> deleteTask({
+    required DeleteTaskModel payload,
+  });
+}
