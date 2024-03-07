5a6b9239f85aeb6d285c98c72a3a1a3637c3aa00
Cashkid500 <murtadhob@gmail.com>
2024-03-07 17:37:30 +0100

    Repository abstract files created

---
+++ lib/data/repository/tasks/tasks_abstract.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:task_manager/core/failures/failures.dart';
+import 'package:task_manager/models/tasks/body/create_task.dart';
+import 'package:task_manager/models/tasks/body/delete_task.dart';
+import 'package:task_manager/models/tasks/body/update_task.dart';
+import 'package:task_manager/models/tasks/response/create_task.dart';
+import 'package:task_manager/models/tasks/response/delete_task.dart';
+import 'package:task_manager/models/tasks/response/update_task.dart';
+
+//************ Create Task **********
+abstract class CreateTaskRepo {
+  Future<Either<Failure, CreateTaskResponse>> createTask({
+    required CreateTaskModel payload,
+  });
+}
+
+//************ Update Task **********
+abstract class UpdateTaskRepo {
+  Future<Either<Failure, UpdateTaskResponse>> updateTask({
+    required UpdateTaskModel payload,
+  });
+}
+
+//************ Delete Task **********
+abstract class DeleteTaskRepo {
+  Future<Either<Failure, DeletetaskResponse>> deleteTask({
+    required DeleteTaskModel payload,
+  });
+}
