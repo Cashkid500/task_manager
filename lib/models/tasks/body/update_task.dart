e0cffe4d50259fcafef275192a7012fecf6e3c9e
Balogun Murtadho <murtadhob@gmail.com>
2024-03-07 12:26:37 +0100

    Update task model created

---
+++ lib/models/tasks/body/update_task.dart
@@ -0,0 +1,8 @@
+class UpdateTaskModel {
+  final String trackid;
+  final String title;
+  final String description;
+  final String date;
+
+  UpdateTaskModel({required this.trackid, required this.title, required this.description, required this.date});
+}
