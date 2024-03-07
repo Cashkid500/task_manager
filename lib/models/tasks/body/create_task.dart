8840420716c10887463c6048715467d5dc7fdbfa
Balogun Murtadho <murtadhob@gmail.com>
2024-03-07 12:26:04 +0100

    Create task model created

---
+++ lib/models/tasks/body/create_task.dart
@@ -0,0 +1,7 @@
+class CreateTaskModel {
+  final String title;
+  final String description;
+  final String date;
+
+  CreateTaskModel({required this.title, required this.description, required this.date});
+}
