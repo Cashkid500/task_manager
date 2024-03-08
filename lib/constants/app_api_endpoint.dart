1ada52570baacfa6ae8e29d1af3a6921228a5fd1
Cashkid500 <murtadhob@gmail.com>
2024-03-08 18:08:51 +0100

    Created a variable constants for API endpoints to ensure code readability and maintainability

---
+++ lib/constants/app_api_endpoint.dart
@@ -0,0 +1,14 @@
+class AppEndpoints {
+  static const _base = 'https://taskmanager3.000webhostapp.com/task_manager_api/api/user/task';
+
+  //************** Create Task *************/
+  static const createTask = '$_base/create_task.php';
+  
+  //************** Update Task *************/
+  static const updateTask = '$_base/update_task.php';
+
+  //************** Delete Task *************/
+  static const deleteTask = '$_base/delete_task.php';
+
+  
+}
