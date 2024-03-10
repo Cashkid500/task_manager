8476b5886eead9fa1d80b75f16ca93379fc780b6
Balogun Murtadho <murtadhob@gmail.com>
2024-03-11 00:09:25 +0100

    Tasks network provider file created

---
+++ lib/providers/network_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:task_manager/core/network/network_info.dart';
+import 'package:task_manager/core/network/network_request.dart';
+import 'package:task_manager/core/network/network_retry.dart';
+
+
+final networkInfoProvider = Provider<NetworkInfo>((ref) => NetworkInfoImpl());
+final networkRequestProvider =
+    Provider<NetworkRequest>((ref) => NetworkRequestImpl());
+// provider to create the instant and for dependency injection
+final networkRetryProvider =
+    Provider<NetworkRetry>((ref) => NetworkRetryImpl());
