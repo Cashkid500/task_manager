0d10ca5f1fa25ea590cd530eec4a10e9b6e328b0
Balogun Murtadho <murtadhob@gmail.com>
2024-03-11 00:10:53 +0100

    Tasks source provider file created

---
+++ lib/providers/source_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:task_manager/data/source/tasks/tasks_abstract.dart';
+import 'package:task_manager/data/source/tasks/tasks_source.dart';
+import 'package:task_manager/providers/network_provider.dart';
+
+//************ Create Task Source Provider **********
+final createTaskSourceProvider = Provider<CreateTaskSource>(
+  (ref) => CreateTaskSourceImpl(
+    networkRequest: ref.read(networkRequestProvider), 
+    networkRetry: ref.read(networkRetryProvider),
+    ),
+  );
+
+//************ Update Task Source Provider **********
+final updateTaskSourceProvider = Provider<UpdateTaskSource>(
+  (ref) => UpdateTaskSourceImpl(
+    networkRequest: ref.read(networkRequestProvider),
+    networkRetry: ref.read(networkRetryProvider),
+    ),
+  );
+
+//************ Delete Task Source Provider **********
+final deleteTaskSourceProvider = Provider<DeleteTaskSource>(
+  (ref) => DeleteTaskSourceImpl(
+    networkRequest: ref.read(networkRequestProvider), 
+    networkRetry: ref.read(networkRetryProvider),
+    ),
+  );
