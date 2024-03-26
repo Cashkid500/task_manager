725a287f9b75b01b451d35938f0aca1dffad6577
Cashkid500 <murtadhob@gmail.com>
2024-03-26 10:53:07 +0100

    Created a reusable widget for home screen

---
+++ lib/screens/home/widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task_manager/constants/asset_path.dart';
+import 'package:task_manager/constants/text_constants.dart';
+
+class RichTextWidget extends StatelessWidget {
+  const RichTextWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: [
+        TextSpan(
+          text: TaskManagerText.task,
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 20.sp,
+            fontWeight: FontWeight.normal,
+            fontFamily: TaskManagerAssetsPath.taskManagerFont,
+          ),
+        ),
+        TextSpan(
+          text: TaskManagerText.manager,
+          style: TextStyle(
+            color: Colors.redAccent,
+            fontSize: 20.sp,
+            fontWeight: FontWeight.normal,
+            fontFamily: TaskManagerAssetsPath.taskManagerFont,
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class PendingTaskWidget extends StatelessWidget {
+  const PendingTaskWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40.sp,
+      width: 310.sp,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(240, 240, 240, 1.0),
+        borderRadius: BorderRadius.circular(10.sp),
+      ),
+      child: Center(
+        child: Text(
+          TaskManagerText.pending,
+          style: TextStyle(
+            fontSize: 15.sp,
+            color: Colors.blueGrey,
+            fontFamily: TaskManagerAssetsPath.taskManagerFont,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ),
+    );
+  }
+}
