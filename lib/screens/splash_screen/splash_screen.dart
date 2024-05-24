0159846f873a6e1e72477f879699aa5815029fd4
BALOGUN MURTADHO <murtadhob@gmail.com>
2024-05-24 09:09:50 +0100

    Implemented the splash screen

---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task_manager/constants/color_constants.dart';
+import 'package:task_manager/constants/text_constants.dart';
+import 'package:task_manager/constants/asset_path.dart';
+import 'package:task_manager/screens/home/home.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(color: whiteText),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100.sp,
+              width: 100.sp,
+              decoration: BoxDecoration(
+                color: deathVictorious,
+                borderRadius: BorderRadius.circular(100.sp),
+              ),
+              child: Icon(
+                Icons.done_sharp,
+                size: 50.sp,
+                color: greenTeal,
+              ),
+            ),
+            SizedBox(
+              height: 10.sp,
+            ),
+            Text(TaskManagerText.taskmanager,
+                style: TextStyle(
+                  fontSize: 25.sp,
+                  fontWeight: FontWeight.w800,
+                  color: blackOut,
+                  fontFamily: TaskManagerAssetsPath.taskManagerFont,
+                ),
+                textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+    );
+  }
+}
